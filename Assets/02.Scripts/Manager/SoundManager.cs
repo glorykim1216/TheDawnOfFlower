45cc82c52329fbbe3c2dbd463c7ff6df47e8db95
glorykim <glorykim1216@gmail.com>
2019-02-01 21:35:08 +0900

    사운드 매니저 수정

--- Assets/02.Scripts/Manager/SoundManager.cs
+++ Assets/02.Scripts/Manager/SoundManager.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class SoundManager : MonoSingleton<SoundManager>
 {
-    static int soundMaxNum = 5; // 최대 동시 사운드 출력 갯수
-    public AudioSource[] audioSource = new AudioSource[soundMaxNum]; 
+    static int soundMaxNum = 5; // 사운드 동시 출력 최대 갯수
+    public AudioSource[] audioSource = new AudioSource[soundMaxNum];
     private Dictionary<string, AudioClip> DicAudioClip = new Dictionary<string, AudioClip>();
 
     public void LoadSound()
     {
+        // 사운드 동시 출력 최대 갯수 만큼 AudioSource 생성
+        for (int i = 0; i < soundMaxNum; i++)
+        {
+            audioSource[i] = this.gameObject.AddComponent<AudioSource>();
+        }
+
+        // 사운드 로드
         AudioClip[] resourcesAudioClip = Resources.LoadAll<AudioClip>("Sound");
         for (int i = 0; i < resourcesAudioClip.Length; i++)
         {
@@ -23,7 +30,7 @@ public class SoundManager : MonoSingleton<SoundManager>
         for (int i = 0; i < soundMaxNum; i++)
         {
             // 재생 중이 아닌 오디오 소스 탐색
-            if(audioSource[i].isPlaying == false)
+            if (audioSource[i].isPlaying == false)
             {
                 //오디오 클립 변경
                 audioSource[i].clip = DicAudioClip[_clip];
@@ -48,10 +55,10 @@ public class SoundManager : MonoSingleton<SoundManager>
         for (int i = 0; i < soundMaxNum; i++)
         {
             // 재생 중인 오디오 소스 탐색
-            if(audioSource[i].isPlaying == true)
+            if (audioSource[i].isPlaying == true)
             {
                 // 오디오 클립 이름 확인
-                if(audioSource[i].clip.name == _clip)
+                if (audioSource[i].clip.name == _clip)
                 {
                     // 정지
                     audioSource[i].Stop();
